a0ae843f051f4892cc7465ec6318b3ccf4bd034b
ericgd3d <77587700+ericgd3d@users.noreply.github.com>
2023-07-14 11:19:10 -0400

    Added Maya menu and alembic proxy

---
+++ tonic_alembic.py
@@ -0,0 +1,5 @@
+
+def exportAbc():
+    import maya.cmds as cmds
+    import maya.mel as mel
+    print('Exporting Abc')
---
+++ tonic_menu.py
@@ -0,0 +1,24 @@
+"""
+Contains the menu creation functions as wells as any other functions the menus rely on.
+"""
+
+import maya.cmds as cmds
+import maya.mel as mel
+
+GC_PROTECT = []
+
+def create_menu():
+    import sys
+
+    """Creates the CMT menu."""
+    gmainwindow = mel.eval('$tmp = $gMainWindow;')
+    menu = cmds.menu(parent=gmainwindow, tearOff=True, label='TonicDNA_Menu')
+
+    tonic_anim_menu = cmds.menuItem(subMenu=True, tearOff=True, parent=menu, label='Animation')
+
+    cmds.menuItem(parent=tonic_anim_menu,
+                  label='Export to Alembic',
+                  #command='import tonic_exportAbcGUI;mainwindow=tonic_exportAbcGUI.MainWindow()',
+                  command='import tonic_alembic; tonic_alembic.exportAbc())',
+                  imageOverlayLabel='ExportAbc')
+
