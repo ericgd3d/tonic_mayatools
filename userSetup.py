fe07fefd236e64e4d7508b35b7b81127424c6aab
Eric Gervais-Despres <eric,gervais-despres@tonicdna.com>
2023-11-08 18:02:34 -0500

    calls the openerMaya to set the menu hijack and create the tonicNode

--- userSetup.py
+++ userSetup.py
@@ -7,3 +7,10 @@ print ('* * * loading oldest pipeline tools * * * ')
 import tonic_menu
 cmds.evalDeferred("tonic_menu.create_menu()")
 
+try:
+    import tonic_openerMaya
+    cmds.evalDeferred("tonic_openerMaya.tonic_openerMaya()")
+except ImportError:
+    pass
+
+    
