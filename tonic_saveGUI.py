9419dc48d798b86e0f16b88d1d5b1d5b6349a0ad
Eric Gervais-Despres <eric,gervais-despres@tonicdna.com>
2023-11-08 18:01:14 -0500

    just before getting the template to set the path

--- tonic_saveGUI.py
+++ tonic_saveGUI.py
@@ -1,7 +1,12 @@
 import sys
 from maya import OpenMayaUI as mui
 from shiboken2 import wrapInstance
-from PySide2.QtWidgets import QApplication, QDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QRadioButton, QWidget
+from PySide2.QtWidgets import QApplication, QDialog, QFileDialog, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QRadioButton, QWidget, QFrame, QSpacerItem, QSizePolicy
+from PySide2.QtCore import Qt
+from PySide2 import QtGui, QtCore
+import sg_utils
+import tonic_nodeUtil
+import os
 
 class TonicSaveDialog(QDialog):
     def __init__(self, parent=None):
@@ -25,6 +30,28 @@ class TonicSaveDialog(QDialog):
         lbl_logo.setScaledContents(True)
         top_layout.addWidget(lbl_logo)
 
+        server = os.environ["TONIC_SERVER"]
+        self.current_sgtask = None
+
+        if server:
+            self.sg = sg_utils.get_sg(server)
+            self.current_sgtask = tonic_nodeUtil.tonic_get_current_sgtask(self.sg)
+
+        if self.current_sgtask:
+            #print(self.current_sgtask)
+            next_version_number = 1
+            published_files = sg_utils.tonic_get_publish_files(self.sg, self.current_sgtask)
+            if published_files:
+                latest_publish = published_files[0]
+                latest_version_number = latest_publish['version_number']
+                if latest_version_number:
+                    next_version_number = latest_version_number + 1
+
+
+
+        #latest_publish_file = sg_utils.tonic_get_latest_publishfile
+        #next_publish_file =
+
         lbl_filename = QLabel("filename")
         lbl_filename.setFont(QtGui.QFont("", 18))
         lbl_filename.setAlignment(QtCore.Qt.AlignCenter)
@@ -39,26 +66,30 @@ class TonicSaveDialog(QDialog):
 
 
 
-        rb_wip = QRadioButton("Save New Version of Work in Progress")
-        rb_approval = QRadioButton("Save New Version and Send for Approval")
+        self.rb_wip = QRadioButton("Save New Version of Work in Progress")
+        self.rb_approval = QRadioButton("Save New Version and Send for Approval")
 
-        radio_layout.addWidget(rb_wip)
-        radio_layout.addWidget(rb_approval)
+        radio_layout.addWidget(self.rb_wip)
+        radio_layout.addWidget(self.rb_approval)
 
         layout.addLayout(radio_layout)
 
         path_layout = QHBoxLayout()
         path_layout.setContentsMargins(100, 0, 0, 10)
 
-        lbl_media = QLabel("Path of Media:")
-        le_path = QLineEdit()
-        btn_browse = QPushButton("Browse")
+        self.lbl_media = QLabel("Path of Media:")
+        self.le_path = QLineEdit()
+        self.btn_browse = QPushButton("Browse")
+        self.btn_browse.clicked.connect(self.browse_file)
 
-        path_layout.addWidget(lbl_media)
-        path_layout.addWidget(le_path)
-        path_layout.addWidget(btn_browse)
+        path_layout.addWidget(self.lbl_media)
+        path_layout.addWidget(self.le_path)
+        path_layout.addWidget(self.btn_browse)
 
         layout.addLayout(path_layout)
+        self.rb_wip.setChecked(True)  # Set the first radio button to be selected by default
+        self.rb_approval.toggled.connect(self.tonic_make_browser_enable)
+        self.tonic_make_browser_enable()
 
 
         # Create a separator (QFrame) between "Path of Media" and "Comments"
@@ -93,6 +124,8 @@ class TonicSaveDialog(QDialog):
 
         btn_save = QPushButton("Save")
         btn_cancel = QPushButton("Cancel")
+        btn_cancel.clicked.connect(self.tonic_cancel)
+
 
         button_layout.addWidget(btn_save)
         button_layout.addWidget(btn_cancel)
@@ -101,11 +134,45 @@ class TonicSaveDialog(QDialog):
 
         msg_layout = QHBoxLayout()
         msg_layout.setContentsMargins(10, 0, 0, 0)
-        lbl_message = QLabel("message")
+        lbl_message = QLabel("")
         msg_layout.addWidget(lbl_message)
         layout.addLayout(msg_layout)
 
+
+
         self.setLayout(layout)
+
+    def tonic_cancel(self):
+        print('cancel')
+        self.close()
+
+    def tonic_make_browser_enable(self):
+        if self.rb_approval.isChecked():
+            print('appr')
+            self.lbl_media.setText("Path of Media:")
+            self.le_path.setVisible(True)
+            self.btn_browse.setVisible(True)
+        else:
+            print('wip')
+            self.lbl_media.setText(" ")
+            self.le_path.setVisible(False)
+            self.btn_browse.setVisible(False)
+
+    def browse_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly  # Make sure the user can only select existing files
+
+        file_path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Select a File",
+            "",  # Provide a directory path to start in, or leave it empty for the default location
+            "All Files (*);;Jpeg Files (*.jpg);;Movie Files (*.mov)"  # Define the file filters if needed
+        )
+
+        if file_path:
+            self.le_path.setText(file_path)
+
+
 
 def main():
     app = QApplication.instance()
@@ -117,5 +184,5 @@ def main():
 
     app.exec_()  # Start the event loop
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
