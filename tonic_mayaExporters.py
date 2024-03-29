dbe33f08e75176062a5dc6f7073bf92d89b9e81c
Eric Gervais-Despres <eric,gervais-despres@tonicdna.com>
2024-03-29 12:30:45 -0400

    added usd export

--- tonic_mayaExporters.py
+++ tonic_mayaExporters.py
@@ -1,3 +1,5 @@
+import shutil
+
 DEBUG = True
 
 
@@ -17,6 +19,9 @@ def tonic_create_dependent(tonic_node, outfile):
     elif filetype == 'abc':
         if not cmds.pluginInfo('AbcExport', query=True, loaded=True):
             cmds.loadPlugin('AbcExport')
+    elif filetype == 'usd':
+        if not cmds.pluginInfo('mayaUsdPlugin', query=True, loaded=True):
+            cmds.loadPlugin('mayaUsdPlugin')
 
     # Check if the directory exists, if not, create it
     output_dir = os.path.dirname(outfile)
@@ -37,7 +42,7 @@ def tonic_create_dependent(tonic_node, outfile):
 
         if geo_grp:
             if filetype == 'fbx':
-                cmds.select(geo_grp)
+                cmds.select(geo_grp, replace=True)
                 cmds.file(outfile, force=True, options="v = 0", type="FBX export", exportSelected=True)
                 if os.path.isfile(outfile):
                     #Export worked
@@ -51,6 +56,27 @@ def tonic_create_dependent(tonic_node, outfile):
                 cmds.AbcExport(j=command)
                 if os.path.isfile(outfile):
                     # Export worked
+                    return outfile
+
+            elif filetype == 'usd':
+                cmds.select(geo_grp, replace=True)
+
+                # Set the frame range to the current frame
+                current_frame = cmds.currentTime(query=True)
+                options = ";exportUVs=1;exportSkels=none;exportSkin=none;exportBlendShapes=0;exportColorSets=1;defaultMeshScheme=catmullClark;defaultUSDFormat=usdc;animation=0;eulerFilter=0;staticSingleSample=0;startTime=1;endTime=1;frameStride=1;frameSample=0.0;parentScope=;exportDisplayColor=0;shadingMode=useRegistry;convertMaterialsTo=UsdPreviewSurface;exportInstances=1;exportVisibility=1;mergeTransformAndShape=0;stripNamespaces=1"
+
+                #Creating a temp file for export since it seems to have difficulties saving on QSync directly
+                import tempfile
+                tmpDir = tempfile.gettempdir()
+                tmp_outfile = tmpDir + '/' + os.path.basename(outfile)
+                cmds.file(
+                    tmp_outfile, force=True, exportSelected=True, preserveReferences=True, type='USD Export',
+                    options=options
+                )
+
+                if os.path.isfile(tmp_outfile):
+                    # Export worked
+                    shutil.move(tmp_outfile, outfile)
                     return outfile
 
     return None
